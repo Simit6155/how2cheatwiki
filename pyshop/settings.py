9caf433e40cb5e553d828b78ccf90ee308ae1202
simit <semihbiyikli769@gmail.com>
2025-07-30 23:58:23 +0200

    Update settings.py

--- pyshop/settings.py
+++ pyshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-**2c+7am(ldagd+yjb9ng3aswxo-hhqj)mg@!%d*1sxj34=eo3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".onrender.com"]
 
 
 # Application definition
